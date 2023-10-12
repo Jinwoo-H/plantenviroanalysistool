50d5c8e0b54c980c6964a666abf054f4ee3f45bd
Jinwoo Hong <73622457+Jinwoo-H@users.noreply.github.com>
2023-10-12 18:26:18 -0400

    Update plantenviroanalysistool.ino

--- plantenviroanalysistool.ino
+++ plantenviroanalysistool.ino
@@ -1,50 +1,87 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include "DHT.h"
+#define DHTPIN D4
+#define DHTTYPE DHT11
+#define SOILPIN A0
+DHT dht(DHTPIN, DHTTYPE);
 
-/* Set these to your desired credentials. */
-const char *ssid = "WIFISSID"; //Enter your WIFI ssid
-const char *password = "WIFIPASS"; //Enter your WIFI password
+WiFiClient clientt;
+
+const char *ssid = "WIFI SSID"; //Enter your WIFI ssid
+const char *password = "WIFI PASS"; //Enter your WIFI password
 ESP8266WebServer server(80);
-void handleRoot() {
- server.send(200, "text/html", "<form action=\"/LED_BUILTIN_on\" method=\"get\" id=\"form1\"></form><button type=\"submit\" form=\"form1\" value=\"On\">On</button><form action=\"/LED_BUILTIN_off\" method=\"get\" id=\"form2\"></form><button type=\"submit\" form=\"form2\" value=\"Off\">Off</button>");
-}
-void handleSave() {
- if (server.arg("pass") != "") {
-   Serial.println(server.arg("pass"));
- }
-}
+
+const char *host = "HOST IP ADDRESS"; //host IP address
+ 
+const int dry = 658; //value for soil moisture sensor when dry
+const int wet = 283; //value for soil moisture sensor when wet
 
 void setup() {
- pinMode(LED_BUILTIN, OUTPUT);
- delay(3000);
- Serial.begin(115200);
- Serial.println();
- Serial.print("Configuring access point...");
- WiFi.begin(ssid, password);
- while (WiFi.status() != WL_CONNECTED) {
-   delay(500);
-   Serial.print(".");
- }
- Serial.println("");
- Serial.println("WiFi connected");
- Serial.println("IP address: ");
- Serial.println(WiFi.localIP());
- server.on ( "/", handleRoot );
- server.on ("/save", handleSave);
- server.begin();
- Serial.println ( "HTTP server started" );
- server.on("/LED_BUILTIN_on", []() {
-   digitalWrite(LED_BUILTIN, 0);
-   Serial.println("on");
-   handleRoot();
- });
- server.on("/LED_BUILTIN_off", []() {
-   digitalWrite(LED_BUILTIN, 1);
-   Serial.println("off");
-   handleRoot();
- });
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  Serial.println("DHT Output: ");
+  dht.begin();
+
+  Serial.println();
+  Serial.print("Configuring access point...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
+
+// the loop function runs over and over again forever
 void loop() {
- server.handleClient();
-} 
+
+  HTTPClient http;
+  
+  float temp = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  float soilInt = analogRead(SOILPIN);
+  float percentageSoil = map(soilInt, wet, dry, 100, 0);
+  if(isnan(temp) || isnan(humidity)){
+    Serial.println("Failed to read DHT11");
+  }
+  else{
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%\t");
+    Serial.print("Temperatures: ");
+    Serial.print(temp);
+    Serial.print(" *C ");
+    Serial.print("Moisture: ");
+    Serial.print(percentageSoil);
+    Serial.print("%\t                 ");
+    delay(5000);
+  }
+
+  String getData, link, tempStr, humStr, moistStr;
+
+  tempStr = String(temp);
+  humStr = String(humidity);
+  moistStr = String(percentageSoil);
+  
+  getData = "?Temperature=" + tempStr + "&Humidity=" + humStr + "&Moisture=" + moistStr;
+  link = "http://HOST IP ADDRESS/connect.php" + getData; //change HOST IP ADDRESS with IP address
+
+  http.begin(clientt, link);
+  //begin(WiFiClient, link);
+
+  int httpCode = http.GET();
+  String payload = http.getString();
+
+  Serial.print(httpCode);
+  Serial.print("    ");
+  Serial.println(payload);
+
+  http.end();
+  
+}
